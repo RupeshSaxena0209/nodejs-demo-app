2d7fc5e7c4c33284789da6bf1931df7cddd98699
RupeshSaxena0209 <86105150+RupeshSaxena0209@users.noreply.github.com>
2025-09-22 17:14:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Elevate Lab This is completion of my first task ');
+  res.send('Hello Elevate Lab This is completion of my first task for sample nodejs app ');
 });
 
 app.listen(port, () => {
